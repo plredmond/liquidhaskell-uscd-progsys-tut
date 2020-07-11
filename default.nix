8d6ccdb05d40196ddc4ef49091058e2f625c2467
PLR <51248199+plredmond@users.noreply.github.com>
2020-07-10 18:02:25 -0700

    use 8.10.1 and a more recent doctest

--- default.nix
+++ default.nix
@@ -12,7 +12,15 @@ let
     (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs)
     { inherit config; };
   # optionally, override haskell compiler version and/or dependencies in nixpkgs
-  haskellPackages = nixpkgs.haskellPackages;
+  haskellPackages = nixpkgs.haskell.packages."ghc8101".override (
+    old: {
+      overrides = self: super: {
+        # nixos-20.03 has an old version of doctest and the 0.17 has a test that calls cabal
+        doctest = nixpkgs.haskell.lib.dontCheck
+          (self.callHackageDirect { pkg = "doctest"; ver = "0.16.2"; sha256 = "11py87v0w70x60l2a9grv2vm2kfacczdxhn0rkyvisa4fsan936j"; } {});
+      };
+    }
+  );
   # function to bring in devtools
   devtools = old: { nativeBuildInputs = with nixpkgs.pkgs; old.nativeBuildInputs ++ [ cabal-install ghcid ]; }; # ghc and hpack are automatically included
   # ignore files specified by gitignore
