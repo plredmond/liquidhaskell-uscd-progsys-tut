912778f25a367c9ac18bff7b3e6dca6370a6948a
PLR <51248199+plredmond@users.noreply.github.com>
2020-07-13 12:11:38 -0700

    remove doctest annotations

--- lib/Abcexample.hs
+++ lib/Abcexample.hs
@@ -3,16 +3,6 @@ module Abcexample where
 -- https://www.haskell.org/cabal/users-guide/developing-packages.html#accessing-data-files-from-package-code
 import qualified Paths_abcexample
 
--- $setup
--- >>> import Test.QuickCheck
-
-
--- | Main in the library
---
--- >>> 1 + 1
--- 2
---
--- prop> \(Small n) -> n * 2 == n + n
 main :: IO ()
 main = do
     putStrLn "Hello world!"
